23c1b7b4fc8f586716292cba42728128781586a0
JessicaVF <villarjessica@hotmail.com>
2021-06-24 14:31:56 +0200

    first commit

---
+++ core/Controllers/Home.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Controllers;
+
+
+class Home //extends Controller si besoin d'un model
+{
+    //  protected $modelName; si besoin d'un model
+
+
+    /**
+     * affiche l'accueil du framework
+     * 
+     * 
+     */
+    public function index()
+    {
+        $titreDeLaPage = "Accueil Framework";
+
+         $message = "Bienvenue dans la doc";
+
+        if(!empty($_POST['message'])){
+            $message = $_POST['message'];
+        }
+
+        \Rendering::render('home/home', compact('titreDeLaPage', 'message'));
+
+
+    }
+
+   
+
+
+
+}
---
+++ templates/home/home.html.php
@@ -0,0 +1,8 @@
+<p>coucou je suis home</p>
+
+<h3><?php echo $message ?></h3>
+
+<form method="POST">
+    <input type="texarea" name="message" placeholder="votre message">
+    <input type="submit" value="submit">
+</form>
